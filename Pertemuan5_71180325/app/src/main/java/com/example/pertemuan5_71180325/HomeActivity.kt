b4ea4ec8867593b9fc2bfa2b50f1ef24d6ad9615
arfianandahartono <arfiananda.dhede@ti.ukdw.ac.id>
2022-03-07 14:49:21 +0700

    pt5
    persiapan

---
+++ Pertemuan5_71180325/app/src/main/java/com/example/pertemuan5_71180325/HomeActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pertemuan5_71180325
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.EditText
+import android.widget.TextView
+
+class HomeActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        val email = intent.getStringExtra("email")
+
+        val txtHome = findViewById<TextView>(R.id.txtHome)
+        txtHome.text = "Halo ${email}"
+    }
+}
--- Pertemuan5_71180325/app/src/main/java/com/example/pertemuan5_71180325/MainActivity.kt
+++ Pertemuan5_71180325/app/src/main/java/com/example/pertemuan5_71180325/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.pertemuan5_71180325
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,12 +12,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val email = findViewById<EditText>(R.id.email)
-        val password = findViewById<EditText>(R.id.password)
-        val login = findViewById<EditText>(R.id.login)
+        //val password = findViewById<EditText>(R.id.password)
+        val login = findViewById<Button>(R.id.login)
 
         login.setOnClickListener{
-
+            val intent = Intent(this, HomeActivity::class.java)
+            intent.putExtra("email", email.text.toString())
+            startActivity(intent)
         }
-
     }
 }
