ab328c4c8ec2ad9b64685f19ff9e9f28580a9596
arfianandahartono <arfiananda.dhede@ti.ukdw.ac.id>
2022-03-07 11:24:04 +0700

    perbaikan akhir
    Perbaikan lagi

--- Pertemuan4_71180325/app/src/main/java/com/example/pertemuan4/MainActivity.kt
+++ Pertemuan4_71180325/app/src/main/java/com/example/pertemuan4/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.pertemuan4
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
--- Pertemuan5_71180325/app/src/main/java/com/example/pertemuan5_71180325/MainActivity.kt
+++ Pertemuan5_71180325/app/src/main/java/com/example/pertemuan5_71180325/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.pertemuan5_71180325
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val login = findViewById<EditText>(R.id.login)
+
+        login.setOnClickListener{
+
+        }
+
     }
 }
